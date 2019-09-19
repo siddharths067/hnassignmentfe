26cbf6a3f4d26847d3e4193b341b0c020e6fcecb
Siddharth Singh <siddharths067@gmail.com>
2019-09-19 17:51:55 +0530

    Logout Functionality added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,239 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import SignIn from "./SignIn";
+import cookie from 'react-cookies'
+import Button from "@material-ui/core/Button";
+
+
+function urlencodeFormData(fd) {
+    var s = '';
+
+    function encode(s) {
+        return encodeURIComponent(s).replace(/%20/g, '+');
+    }
+
+    for (var pair of fd.entries()) {
+        if (typeof pair[1] == 'string') {
+            s += (s ? '&' : '') + encode(pair[0]) + '=' + encode(pair[1]);
+        }
+    }
+    return s;
+}
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <SignIn/>;
+    }
+}
+
+
+class CommentPaper extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.upvote = this.upvote.bind(this);
+        this.downvote = this.downvote.bind(this);
+        this.recalibrateVotes = this.recalibrateVotes.bind(this);
+    }
+
+    recalibrateVotes() {
+        var data = new FormData();
+        data.append(`id`, this.props.replyto);
+        data.append(`HNToken`, cookie.load(`HNToken`));
+        console.log(cookie.load(`HNToken`));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'http://127.0.0.1:8000/api/comment/vote/get', false);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xhr.send(urlencodeFormData(data));
+        const resp = JSON.parse(xhr.responseText);
+        this.setState({
+            upvotes: resp.upvotes,
+            downvotes: resp.downvotes
+        });
+        xhr.onerror = function () {
+            console.log(`Error has occurred`);
+        }
+    }
+
+    componentDidMount() {
+        this.recalibrateVotes();
+    }
+
+    upvote() {
+        var data = new FormData();
+        data.append(`id`, this.props.replyto);
+        data.append('type', 'upvote');
+        data.append(`HNToken`, cookie.load(`HNToken`));
+        console.log(cookie.load(`HNToken`));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'http://127.0.0.1:8000/api/comment/vote', false);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xhr.send(urlencodeFormData(data));
+        const resp = JSON.parse(xhr.responseText);
+        this.setState({
+            upvotes: parseInt(this.state.upvotes) + 1
+        });
+        xhr.onerror = function () {
+            console.log(`Error has occurred`);
+        };
+        this.recalibrateVotes();
+    }
+
+    downvote() {
+        var data = new FormData();
+        data.append(`id`, this.props.replyto);
+        data.append('type', 'downvote');
+        data.append(`HNToken`, cookie.load(`HNToken`));
+        console.log(cookie.load(`HNToken`));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'http://127.0.0.1:8000/api/comment/vote', false);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xhr.send(urlencodeFormData(data));
+        const resp = JSON.parse(xhr.responseText);
+        this.setState({
+            downvotes: parseInt(this.state.downvotes) + 1
+        });
+        xhr.onerror = function () {
+            console.log(`Error has occurred`);
+        };
+        this.recalibrateVotes();
+    }
+
+    render() {
+        return <Paper>
+            <h2> {this.props.text}</h2>
+            <Paper>
+                {`By ${this.props.username}`}
+                <a href={`/feed/${this.props.replyto}`}>
+                    <Button variant="contained">
+                        See Replies or Reply Here to in a Nested Thread
+                    </Button>
+                </a>
+                <Button variant="contained" onClick={this.upvote}>
+                    UPVOTE {(this.state.upvotes) ? this.state.upvotes : ""}
+                </Button>
+                <Button variant="contained" onClick={this.downvote}>
+                    DOWNVOTE {(this.state.downvotes) ? this.state.downvotes : ""}
+                </Button>
+            </Paper>
+        </Paper>;
+    }
+}
+
+
+class Feed extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            replyto: this.props.match.params.replyto
+        };
+
+        this.commentSubmit = this.commentSubmit.bind(this);
+        this.softRefresh = this.softRefresh.bind(this);
+        this.logout = this.logout.bind(this);
+    }
+
+    softRefresh() {
+        console.log(10000);
+        this.setState({
+            comments: 10000
+        });
+
+        var data = new FormData();
+        data.append(`replyto`, this.props.match.params.replyto);
+        data.append(`HNToken`, cookie.load(`HNToken`));
+        console.log(cookie.load(`HNToken`));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'http://127.0.0.1:8000/api/comment/get', false);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xhr.send(urlencodeFormData(data));
+        this.setState({
+            comments: JSON.parse(xhr.responseText)
+        });
+        xhr.onerror = function () {
+            console.log(`Error has occurred`);
+        }
+    }
+
+    componentDidMount() {
+        this.softRefresh();
+    }
+
+    commentSubmit(event) {
+
+        event.preventDefault();
+        var data = new FormData();
+        data.append(`text`, event.target.text.value);
+        data.append('replyto', this.props.match.params.replyto);
+        data.append(`HNToken`, cookie.load(`HNToken`));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'http://127.0.0.1:8000/api/comment', false);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xhr.send(urlencodeFormData(data));
+        xhr.onerror = function () {
+            console.log(`Error has occurred`);
+        };
+
+        this.softRefresh();
+
+    }
+
+    logout() {
+        var data = new FormData();
+        data.append(`HNToken`, cookie.load(`HNToken`));
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'http://127.0.0.1:8000/api/user/logout', false);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+        xhr.send(urlencodeFormData(data));
+        console.log(xhr.responseText);
+        xhr.onerror = function () {
+            console.log(`Error has occurred`);
+        };
+
+        window.location.href = "/";
+    }
+
+    render() {
+        return <Paper>
+            <form onSubmit={this.commentSubmit}>
+                <input type={"text"} name={"text"} id={"text"}/>
+                <input type={"submit"} name={"submit"}
+                       value={(this.state.replyto === "-1") ? "POST a new Thread" : "Reply to this Top Comment"}/>
+            </form>
+
+            <Button variant="contained" onClick={this.logout}>
+                Logout
+            </Button>
+
+            {(!this.state.comments) ? "Please wait while the components load" : this.state.comments['comments'].map(e =>
+                <CommentPaper text={e.text} username={e.username} replyto={e.id}/>)}
+        </Paper>;
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Route path={"/"} exact component={Login}/>
+            <Route path={"/feed/:replyto"} component={Feed}/>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/SignIn.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import cookie from 'react-cookies'
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+        body: {
+            backgroundColor: theme.palette.common.white,
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+function handleRegister(event) {
+    console.log(`Register Called`);
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(username);
+    console.log(password);
+    event.preventDefault();
+}
+
+function urlencodeFormData(fd) {
+    var s = '';
+
+    function encode(s) {
+        return encodeURIComponent(s).replace(/%20/g, '+');
+    }
+
+    for (var pair of fd.entries()) {
+        if (typeof pair[1] == 'string') {
+            s += (s ? '&' : '') + encode(pair[0]) + '=' + encode(pair[1]);
+        }
+    }
+    return s;
+}
+
+function handleSubmit(event) {
+    console.log(event.target);
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+    console.log(username);
+    console.log(password);
+    // Login
+
+    var data = new FormData();
+    data.append(`username`, username);
+    data.append(`password`, password);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', 'http://127.0.0.1:8000/api/user/login', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    xhr.send(urlencodeFormData(data));
+    xhr.onload = function () {
+        // do something to response
+        //console.log(this.responseText);
+        const jsResponse = JSON.parse(this.responseText);
+        //alert(`The Auth Token is ${jsResponse.token}`);
+        cookie.save(`HNToken`, jsResponse.token, {path: `/`});
+        //console.log(cookie.load(`HNToken`));
+    };
+    xhr.onerror = function () {
+        alert("Please Try Again");
+    };
+
+    event.preventDefault();
+}
+
+export default function SignIn() {
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form id={"credForm"} className={classes.form} onSubmit={handleSubmit} method={"POST"} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Grid container>
+                        <Grid item xs>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                                name={"login"}
+                                value={"login"}
+                            >
+                                Sign In
+                            </Button>
+                        </Grid>
+                        <Grid item xs>
+                            <Button
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                                name={"register"}
+                                value={"register"}
+                                onClick={handleRegister}
+                            >
+                                Register
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright/>
+            </Box>
+        </Container>
+    );
+}
